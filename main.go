c886894d8677846c381df24ea2b5cb6599b1b8e4
heronh <heronhurpia@gmail.com>
2025-06-13 13:44:28 -0300

    Versão intermediária de teste

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 )
 
 // Variável final para os pacotes BAT
-var final []BATSection
+var finalBATSection []BATSection
 
 func main() {
 
@@ -72,6 +72,7 @@ func main() {
 			sectionBuffer = append(sectionBuffer, payload...)
 			if len(sectionBuffer) >= expectedLength {
 				section := parseBATSection(sectionBuffer)
+				addSectionToFinal(section)
 				if section != nil && section.BouquetID == 25008 {
 					fmt.Printf("Parsed BAT Section: Bouquet ID: %d, Version: %d\n", section.BouquetID, section.VersionNumber)
 					for _, ts := range section.TransportStreams {
@@ -91,8 +92,114 @@ func main() {
 		}
 	}
 
-	// Report final BAT sections
-	fmt.Printf("Total BAT sections collected: %d\n", len(final))
+	// order finalBATSection by Bouquet ID
+	for i := 0; i < len(finalBATSection)-1; i++ {
+		for j := i + 1; j < len(finalBATSection); j++ {
+			if finalBATSection[i].BouquetID > finalBATSection[j].BouquetID {
+				finalBATSection[i], finalBATSection[j] = finalBATSection[j], finalBATSection[i]
+			}
+		}
+	}
+	fmt.Println("Final BAT Sections:")
+	for _, section := range finalBATSection {
+
+		if section.BouquetID != 25008 {
+			continue
+		}
+
+		fmt.Printf("\nBouquet ID: %d, Version: %d, Section: %d/%d\n", section.BouquetID, section.VersionNumber, section.SectionNumber, section.LastSectionNumber)
+		for _, ts := range section.TransportStreams {
+			fmt.Printf("\tTS ID: %d, ONID: %d, Descriptors: %d\n", ts.TransportStreamID, ts.OriginalNetworkID, len(ts.TransportDescriptors))
+			for _, desc := range ts.TransportDescriptors {
+				if desc.Tag == otaLinkageDescriptor {
+					fmt.Printf("\t\tOTA Linkage Descriptor found %d:\n\t\t", desc.Tag)
+					fmt.Println(hex.Dump(desc.Data))
+				}
+			}
+		}
+		for _, desc := range section.BouquetDescriptors {
+			if desc.Tag == otaLinkageDescriptor {
+				fmt.Printf("\tOTA Linkage Descriptor found in Bouquet Descriptors 0x%X:\n\t\t", desc.Tag)
+				fmt.Println(hex.Dump(desc.Data))
+			}
+		}
+		break
+	}
+
+}
+
+func addSectionToFinal(section *BATSection) {
+
+	// If the section is nil, do not add it
+	if section == nil {
+		return
+	}
+
+	// Check if the section with same bouquetid exists in the final list
+	found := false
+	for _, existingSection := range finalBATSection {
+		if existingSection.BouquetID == section.BouquetID {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		// Create a new empty final section
+		newSection := BATSection{
+			BouquetID:         section.BouquetID,
+			VersionNumber:     section.VersionNumber,
+			SectionNumber:     section.SectionNumber,
+			LastSectionNumber: section.LastSectionNumber,
+			//BouquetDescriptors: section.BouquetDescriptors,
+			//TransportStreams: section.TransportStreams,
+		}
+		finalBATSection = append(finalBATSection, newSection)
+	}
+
+	// Create a pointer to section of this bouquetid
+	var finalSection *BATSection
+	for i := range finalBATSection {
+		if finalBATSection[i].BouquetID == section.BouquetID {
+			finalSection = &finalBATSection[i]
+			break
+		}
+	}
+
+	// Check each BouquetDescriptor and append it to the final section
+	found = false
+	for _, desc := range section.BouquetDescriptors {
+		if desc.Tag == otaLinkageDescriptor {
+			found = true
+			finalSection.BouquetDescriptors = append(finalSection.BouquetDescriptors, desc)
+		}
+	}
+
+	if !found {
+		fmt.Printf("No OTA Linkage Descriptor found in Bouquet Descriptors for Bouquet ID %d\n", section.BouquetID)
+	}
+
+	// Check each TransportStream and append it to the final section
+	for _, ts := range section.TransportStreams {
+		found = false
+		for _, existingTS := range finalSection.TransportStreams {
+			if existingTS.TransportStreamID == ts.TransportStreamID && existingTS.OriginalNetworkID == ts.OriginalNetworkID {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			newTS := TransportStream{
+				TransportStreamID:    ts.TransportStreamID,
+				OriginalNetworkID:    ts.OriginalNetworkID,
+				TransportDescriptors: ts.TransportDescriptors,
+			}
+			finalSection.TransportStreams = append(finalSection.TransportStreams, newTS)
+		} else {
+			fmt.Printf("Transport Stream ID %d already exists in final section for Bouquet ID %d\n", ts.TransportStreamID, section.BouquetID)
+		}
+	}
 }
 
 // Open the TS file
